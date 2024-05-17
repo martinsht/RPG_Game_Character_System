9039fc161d37e88b6af8a5201dc6a139639420aa
mshterev25 <martin.velislav@gmail.com>
2024-05-17 08:45:05 +0300

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
+import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
+        try{
         Character ch1 = new Warrior("Sir Vicious",50,65);
         Character ch2 = new Mage("Tara",80,"medium");
         Character ch3 = new Rogue("Lofty","slow","clumsy");
         System.out.println(ch1.getName()+" attacks "+ch2.getName());
+        TimeUnit.SECONDS.sleep(3);// I delayed the responses to look more like a game.
         ch1.attack();
         ch2.defend();
         System.out.println();
-        System.out.println(ch2.getName()+" attacks "+ch3.getName());
-        ch2.castSpell();
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println(ch2.getName()+" attacks "+ch3.getName());
+            TimeUnit.SECONDS.sleep(3);
+            ch2.castSpell();
         ch3.defend();
         ch3.levelUP();
         System.out.println();
-        System.out.println(ch2.getName()+" attacks "+ch1.getName());
-        ch2.castSpell();
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println(ch2.getName()+" attacks "+ch1.getName());
+            TimeUnit.SECONDS.sleep(3);
+
+            ch2.castSpell();
         ch1.defend();
         ch2.levelUP();
+    } catch (InterruptedException e) {
+        System.err.format("IOException: %s%n", e);
+    }
     }
 }
